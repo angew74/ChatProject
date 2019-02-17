fa7842d8e0e2222635cccd9a6cb64080872ce5f1
nicolò ruberto <agnew74@gmail.com>
2019-02-17 19:38:28 +0100

    caricamento web app e aggiornamento servizi chat app angular quasi funzionante

---
+++ DeltaChat/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('Bisogna scriver codice', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`Delta Chat'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('my-dream-app');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Benvenuto in Delta Chat!');
+  });
+});
---
+++ DeltaChat/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from './core/api.service';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule} from '@angular/router';
+import { ListUserComponent } from './list-user/list-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TokenInterceptor} from './core/interceptor';
+import {DeltaMaterialModule} from './core/material.module';
+import {routing} from './app.routing';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    LoginComponent,
+    ChatComponent,
+    ListUserComponent,
+    EditUserComponent,
+    AddUserComponent
+  ],
+  imports: [
+    RouterModule,
+    BrowserModule,
+    HttpClientModule,
+    routing,
+    FormsModule,
+    ReactiveFormsModule,
+    DeltaMaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [ApiService, {provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi : true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
